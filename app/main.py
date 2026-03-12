90133d749d1ab96aea0e26d31f7abb05b41856bf
thinh082 <158122294+thinh082@users.noreply.github.com>
2026-03-12 14:47:02 +0700

    .

--- app/main.py
+++ app/main.py
@@ -15,7 +15,8 @@ app.add_middleware(
         "http://127.0.0.1:5500",
         "http://127.0.0.1:5173",
         "http://localhost:8000",
-        "http://127.0.0.1:5501"
+        "http://127.0.0.1:5501",
+        "https://fe-tuan.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
